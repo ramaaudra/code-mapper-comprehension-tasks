7f30681dc195536e7dda03013198151b0d430481
ramaaudra <ramaaudra30.ra@gmail.com>
2025-12-11 17:40:50 +0700

    build: setup vite-tsconfig-paths for alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import react from '@vitejs/plugin-react'
-import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     // Enable tree-shaking and optimization
     minify: 'esbuild',
